22d1eda2a3ffbf1e52b2d8c06efe28f641dce18e
Zirada <185837150+zirada@users.noreply.github.com>
2024-11-10 20:41:13 +0100

    Rename EntityEquipmentUpdateEvent and move it to MixinPlayerEntity

--- src/main/kotlin/net/ccbluex/liquidbounce/event/EventManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/event/EventManager.kt
@@ -126,7 +126,7 @@ val ALL_EVENT_CLASSES: Array<KClass<out Event>> = arrayOf(
     PlayerFluidCollisionCheckEvent::class,
     PlayerSneakMultiplier::class,
     LoreQueryEvent::class,
-    EntityEquipmentChangeEvent::class
+    PlayerEquipmentChangeEvent::class
 )
 
 /**
