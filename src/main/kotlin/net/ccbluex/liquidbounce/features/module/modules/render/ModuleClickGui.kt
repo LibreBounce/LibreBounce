dd0ce25c5ec2c82482c84e69fa118a2f7de0675b
1zuna <marco@ccbluex.net>
2024-11-15 19:22:15 +0100

    fix: instant click-gui
    ClickGUI will now open without animation and delay.

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleClickGui.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleClickGui.kt
@@ -19,11 +19,20 @@
 package net.ccbluex.liquidbounce.features.module.modules.render
 
 import net.ccbluex.liquidbounce.event.EventManager
+import net.ccbluex.liquidbounce.event.events.BrowserReadyEvent
 import net.ccbluex.liquidbounce.event.events.ClickGuiScaleChangeEvent
+import net.ccbluex.liquidbounce.event.events.GameRenderEvent
+import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.integration.VirtualScreenType
 import net.ccbluex.liquidbounce.integration.VrScreen
+import net.ccbluex.liquidbounce.integration.browser.supports.tab.ITab
+import net.ccbluex.liquidbounce.integration.theme.ThemeManager
+import net.ccbluex.liquidbounce.utils.client.asText
+import net.ccbluex.liquidbounce.utils.kotlin.EventPriorityConvention
+import net.minecraft.client.gui.DrawContext
+import net.minecraft.client.gui.screen.Screen
 import org.lwjgl.glfw.GLFW
 
 /**
@@ -43,14 +52,55 @@ object ModuleClickGui :
     @Suppress("UnusedPrivateProperty")
     private val searchBarAutoFocus by boolean("SearchBarAutoFocus", true)
 
+    private var clickGuiTab: ITab? = null
+
     override fun enable() {
         // Pretty sure we are not in a game, so we can't open the clickgui
         if (mc.player == null || mc.world == null) {
             return
         }
 
-        mc.setScreen(VrScreen(VirtualScreenType.CLICK_GUI))
+        mc.setScreen(if (clickGuiTab == null) {
+            VrScreen(VirtualScreenType.CLICK_GUI)
+        } else {
+            ClickScreen()
+        })
         super.enable()
+    }
+
+    @Suppress("unused")
+    private val browserReadyHandler = handler<BrowserReadyEvent>(
+        priority = EventPriorityConvention.OBJECTION_AGAINST_EVERYTHING,
+        ignoreCondition = true
+    ) {
+        clickGuiTab = ThemeManager.openInputAwareImmediate(VirtualScreenType.CLICK_GUI, true) {
+            mc.currentScreen is ClickScreen
+        }.preferOnTop()
+    }
+
+    @Suppress("unused")
+    private val gameRenderHandler = handler<GameRenderEvent>(
+        priority = EventPriorityConvention.OBJECTION_AGAINST_EVERYTHING,
+        ignoreCondition = true
+    ) {
+        // A hack to prevent the clickgui from being drawn
+        if (mc.currentScreen !is ClickScreen) {
+            clickGuiTab?.drawn = true
+        }
+    }
+
+    /**
+     * An empty screen that acts as hint when to draw the clickgui
+     */
+    class ClickScreen : Screen("ClickGUI".asText()) {
+        override fun render(context: DrawContext?, mouseX: Int, mouseY: Int, delta: Float) {
+            super.render(context, mouseX, mouseY, delta)
+        }
+
+        override fun shouldPause(): Boolean {
+            // preventing game pause
+            return false
+        }
     }
 
 }
