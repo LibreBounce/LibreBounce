86869d1f673ad73f277338dabf09c7b7543baf16
ccetl <zcvgih@gmail.com>
2024-11-26 21:44:17 +0100

    feat: anti VFP disconnect

---
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/gui/MixinScoreboard.java
@@ -0,0 +1,40 @@
+package net.ccbluex.liquidbounce.injection.mixins.minecraft.gui;
+
+import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.scoreboard.ScoreboardObjective;
+import net.minecraft.scoreboard.Team;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Fixes a ViaFabricPlus bug where it would not correctly handle the scoreboard data, resulting in a disconnect.
+ * <p>
+ * ViaFabricPlus versions: 3.4.8-?
+ */
+@Mixin(Scoreboard.class)
+public abstract class MixinScoreboard {
+
+    @Shadow
+    @Nullable
+    public abstract Team getScoreHolderTeam(String scoreHolderName);
+
+
+    @Redirect(method = "addObjective", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2ObjectMap;containsKey(Ljava/lang/Object;)Z", remap = false))
+    private boolean noCrash(Object2ObjectMap<String, ScoreboardObjective> instance, Object o) {
+        return false;
+    }
+
+    @Inject(method = "removeScoreHolderFromTeam", at = @At("HEAD"), cancellable = true)
+    private void noCrash2(String scoreHolderName, Team team, CallbackInfo ci) {
+        if (getScoreHolderTeam(scoreHolderName) != team) {
+            ci.cancel();
+        }
+    }
+
+}
