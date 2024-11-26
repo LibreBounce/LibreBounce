3a124513e549418b802c57dbf64e6ff95b2f4907
Zirada <185837150+zirada@users.noreply.github.com>
2024-11-26 17:32:15 +0100

    feat(Invsee): view players outside render distance

--- src/main/kotlin/net/ccbluex/liquidbounce/features/command/commands/client/CommandInvsee.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/command/commands/client/CommandInvsee.kt
@@ -3,50 +3,34 @@ package net.ccbluex.liquidbounce.features.command.commands.client
 import com.mojang.blaze3d.systems.RenderSystem
 import net.ccbluex.liquidbounce.features.command.CommandException
 import net.ccbluex.liquidbounce.features.command.builder.*
+import net.ccbluex.liquidbounce.features.module.modules.misc.ModuleInventoryTracker
 import net.ccbluex.liquidbounce.utils.client.*
-import net.minecraft.client.gui.DrawContext
-import net.minecraft.client.gui.screen.ingame.InventoryScreen
-import net.minecraft.client.network.AbstractClientPlayerEntity
-import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.screen.slot.*
+import net.ccbluex.liquidbounce.utils.inventory.ViewedInventoryScreen
+import java.util.*
 
 object CommandInvsee {
 
-    var viewedPlayer: AbstractClientPlayerEntity? = null
+    var viewedPlayer: UUID? = null
 
     fun createCommand() = CommandBuilder.begin("invsee")
         .parameter(
-            ParameterBuilder
-                .begin<String>("name")
-                .verifiedBy(ParameterBuilder.STRING_VALIDATOR)
-                .useMinecraftAutoCompletion()
+            playerParameter()
                 .required()
                 .build()
         )
         .handler { command, args ->
-            val inputName = (args[0] as String)
-            val player = world.players.find { it.nameForScoreboard.equals(inputName, true) }
-                ?: throw CommandException(command.result("playerNotFound", inputName))
+            val inputName = args[0] as String
+            val playerID = network.playerList.find { it.profile.name.equals(inputName, true) }?.profile?.id
+            val player = { world.getPlayerByUuid(playerID) ?: ModuleInventoryTracker.playerMap[playerID] }
+
+            if (playerID == null || player() == null) {
+                throw CommandException(command.result("playerNotFound", inputName))
+            }
 
             RenderSystem.recordRenderCall {
-                mc.setScreen(NoInteractInventory(player))
+                mc.setScreen(ViewedInventoryScreen(player))
             }
-            viewedPlayer = player
+            viewedPlayer = playerID
         }
         .build()
-}
-
-class NoInteractInventory(private var player: PlayerEntity) : InventoryScreen(player) {
-
-    override fun drawBackground(context: DrawContext, delta: Float, mouseX: Int, mouseY: Int) {
-        context.drawTexture(BACKGROUND_TEXTURE, x, y, 0, 0, backgroundWidth, backgroundHeight)
-        drawEntity(
-            context, x + 26, y + 8, x + 75, y + 78,
-            30, 0.0625f, mouseX.toFloat(), mouseY.toFloat(),
-            player
-        )
-    }
-
-    @Suppress("detekt:EmptyFunctionBlock")
-    override fun onMouseClick(slot: Slot?, slotId: Int, button: Int, actionType: SlotActionType?) {}
 }
--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleInventoryTracker.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleInventoryTracker.kt
@@ -6,21 +6,28 @@ import net.ccbluex.liquidbounce.event.events.PlayerEquipmentChangeEvent
 import net.ccbluex.liquidbounce.event.events.WorldChangeEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.command.commands.client.CommandInvsee
-import net.ccbluex.liquidbounce.features.command.commands.client.NoInteractInventory
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.ClientModule
 import net.ccbluex.liquidbounce.features.module.modules.misc.antibot.ModuleAntiBot
+import net.ccbluex.liquidbounce.utils.inventory.ViewedInventoryScreen
 import net.minecraft.client.network.OtherClientPlayerEntity
 import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.EquipmentSlot.MAINHAND
 import net.minecraft.entity.EquipmentSlot.OFFHAND
 import net.minecraft.entity.EquipmentSlot.Type.*
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import java.util.*
 
 object ModuleInventoryTracker : ClientModule("InventoryTracker", Category.MISC) {
 
-    private val playerMap = HashMap<UUID, TrackedInventory>()
+    /** Saves the non-persistent player object associated with the uuid.
+     * This makes it possible to look up inventories of players which aren't in
+     * the render distance. */
+    private val savePlayers by boolean("SavePlayers", false).onChanged { playerMap.clear() }
+
+    private val inventoryMap = HashMap<UUID, TrackedInventory>()
+    val playerMap = HashMap<UUID, PlayerEntity>()
 
     val playerEquipmentChangeHandler = handler<PlayerEquipmentChangeEvent> { event ->
         val player = event.player
@@ -34,7 +41,10 @@ object ModuleInventoryTracker : ClientModule("InventoryTracker", Category.MISC) 
         val mainHandStack = if (updatedSlot == MAINHAND) newItemStack else player.mainHandStack
         val offHandStack = if (updatedSlot == OFFHAND) newItemStack else player.offHandStack
 
-        val trackedInventory = playerMap.getOrPut(player.uuid) { TrackedInventory() }
+        val trackedInventory = inventoryMap.getOrPut(player.uuid) { TrackedInventory() }
+        if (savePlayers) {
+            playerMap[player.uuid] = player
+        }
 
         when (updatedSlot.type) {
             HAND -> {
@@ -64,19 +74,20 @@ object ModuleInventoryTracker : ClientModule("InventoryTracker", Category.MISC) 
     val worldChangeHandler = handler<WorldChangeEvent> { reset() }
 
     private fun reset() {
-        playerMap.keys.forEach { uuid ->
+        inventoryMap.keys.forEach { uuid ->
             val player = world.players.find { it.uuid == uuid } ?: return@forEach
             for (i in 1 until player.inventory.main.size) {
                 player.inventory.main[i] = ItemStack.EMPTY
             }
         }
+        inventoryMap.clear()
         playerMap.clear()
     }
 
     val itemLoreQueryHandler = handler<ItemLoreQueryEvent> { event ->
-        if (!running || mc.currentScreen !is NoInteractInventory) return@handler
-        val player = CommandInvsee.viewedPlayer ?: return@handler
-        val timeStamp = playerMap[player.uuid]?.timeMap?.getLong(event.itemStack)?.takeIf { it != 0L } ?: return@handler
+        if (mc.currentScreen !is ViewedInventoryScreen) return@handler
+        val player = CommandInvsee.viewedPlayer
+        val timeStamp = inventoryMap[player]?.timeMap?.getLong(event.itemStack)?.takeIf { it != 0L } ?: return@handler
         val lastSeen = System.currentTimeMillis() - timeStamp
         event.addLore("§7Last Seen: ${toMinutesSeconds(lastSeen)}§r")
     }
