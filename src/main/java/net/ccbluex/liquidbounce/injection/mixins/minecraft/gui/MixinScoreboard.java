8a9bb33c1eb5d28b59deeb1f884b8e20512ab39c
ccetl <zcvgih@gmail.com>
2024-11-26 22:08:57 +0100

    add an option

--- src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/gui/MixinScoreboard.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/mixins/minecraft/gui/MixinScoreboard.java
@@ -1,6 +1,25 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2015 - 2024 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
 package net.ccbluex.liquidbounce.injection.mixins.minecraft.gui;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.ccbluex.liquidbounce.features.module.modules.player.ModuleAntiExploit;
 import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.scoreboard.Team;
 import org.jetbrains.annotations.Nullable;
@@ -25,12 +44,18 @@ public abstract class MixinScoreboard {
 
     @ModifyExpressionValue(method = "addObjective", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2ObjectMap;containsKey(Ljava/lang/Object;)Z", remap = false))
     private boolean noCrash(boolean original) {
-        return false;
+        var antiExploit = ModuleAntiExploit.INSTANCE;
+        if (antiExploit.getEnabled() && antiExploit.getVfpScoreboardFix()) {
+            return false;
+        } else {
+            return original;
+        }
     }
 
     @Inject(method = "removeScoreHolderFromTeam", at = @At("HEAD"), cancellable = true)
     private void noCrash2(String scoreHolderName, Team team, CallbackInfo ci) {
-        if (getScoreHolderTeam(scoreHolderName) != team) {
+        var antiExploit = ModuleAntiExploit.INSTANCE;
+        if (antiExploit.getEnabled() && antiExploit.getVfpScoreboardFix() && getScoreHolderTeam(scoreHolderName) != team) {
             ci.cancel();
         }
     }
