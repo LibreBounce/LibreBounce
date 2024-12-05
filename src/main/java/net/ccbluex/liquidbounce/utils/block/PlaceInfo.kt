1ada98a529dfd27aed2682fba4dcd2ab11c9e3c6
mems01 <70612656+mems01@users.noreply.github.com>
2024-12-05 23:44:32 +0200

    PlaceInfo

--- src/main/java/net/ccbluex/liquidbounce/utils/block/PlaceInfo.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/block/PlaceInfo.kt
@@ -6,37 +6,21 @@
 package net.ccbluex.liquidbounce.utils.block
 
 import net.ccbluex.liquidbounce.utils.extensions.canBeClicked
+import net.ccbluex.liquidbounce.utils.extensions.center
 import net.minecraft.util.BlockPos
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.Vec3
 
 
-class PlaceInfo(val blockPos: BlockPos, val enumFacing: EnumFacing,
-                var vec3: Vec3 = Vec3(blockPos.x + 0.5, blockPos.y + 0.5, blockPos.z + 0.5)) {
+class PlaceInfo(val blockPos: BlockPos, val enumFacing: EnumFacing, var vec3: Vec3 = blockPos.center) {
 
     companion object {
 
         /**
          * Allows you to find a specific place info for your [blockPos]
          */
-        fun get(blockPos: BlockPos) =
-            when {
-                blockPos.add(0, -1, 0).canBeClicked() ->
-                    PlaceInfo(blockPos.add(0, -1, 0), EnumFacing.UP)
-
-                blockPos.add(0, 0, 1).canBeClicked() ->
-                    PlaceInfo(blockPos.add(0, 0, 1), EnumFacing.NORTH)
-
-                blockPos.add(-1, 0, 0).canBeClicked() ->
-                    PlaceInfo(blockPos.add(-1, 0, 0), EnumFacing.EAST)
-
-                blockPos.add(0, 0, -1).canBeClicked() ->
-                    PlaceInfo(blockPos.add(0, 0, -1), EnumFacing.SOUTH)
-
-                blockPos.add(1, 0, 0).canBeClicked() ->
-                    PlaceInfo(blockPos.add(1, 0, 0), EnumFacing.WEST)
-
-                else -> null
-            }
+        fun get(pos: BlockPos) = EnumFacing.values().find {
+            it != EnumFacing.UP && pos.offset(it).canBeClicked()
+        }?.let { side -> PlaceInfo(pos.offset(side), side.opposite) }
     }
 }
