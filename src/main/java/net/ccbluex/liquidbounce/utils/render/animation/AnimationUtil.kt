9520f9b4d90a6c060f7e2d668b7578482af7a637
mems01 <70612656+mems01@users.noreply.github.com>
2024-12-09 21:32:36 +0200

    feats(legacy): Circle rendering target (Mark) + Option suffix support.

--- src/main/java/net/ccbluex/liquidbounce/utils/render/animation/AnimationUtil.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/render/animation/AnimationUtil.kt
@@ -115,4 +115,11 @@ object AnimationUtil {
         t = t / d - 1
         return c * (t * t * ((s + 1) * t + s) + 1) + b
     }
+
+    fun breathe(duration: Float) = run {
+        // Some cool breathing effects
+        var progress: Float = (System.currentTimeMillis() % duration.toLong()) / duration
+
+         0.5f * (sin(2 * Math.PI * progress) + 1).toFloat()
+    }
 }
