3285366bfe9b9330a30ed00c2687ac5198f8c6f3
DataM0del <183248792+DataM0del@users.noreply.github.com>
2024-12-14 16:20:19 -0600

    fix(FogExtension.kt): detekt (too many parameters, for real this time)

--- src/main/kotlin/net/ccbluex/liquidbounce/utils/client/FogExtension.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/utils/client/FogExtension.kt
@@ -1,17 +1,24 @@
 package net.ccbluex.liquidbounce.utils.client
 
+import net.ccbluex.liquidbounce.render.engine.Color4b
 import net.minecraft.client.render.Fog
 import net.minecraft.client.render.FogShape
+
+// could be merged into `Color4b` as generics, but not sure if izuna would be fine with that.
+data class FloatColor(val r: Float, val g: Float, val b: Float, val a: Float) {
+    constructor(color: Color4b) : this(
+        color.r.toFloat(), color.g.toFloat(),
+        color.b.toFloat(), color.a.toFloat()
+    )
+}
 
 fun Fog.copy(
     positionPair: Pair<Float, Float> = Pair(this.start, this.end),
     shape: FogShape = this.shape,
-    red: Float = this.red,
-    blue: Float = this.blue,
-    green: Float = this.green, alpha: Float = this.alpha
+    color: FloatColor = FloatColor(this.red, this.green, this.blue, this.alpha),
 ): Fog {
     return Fog(
         positionPair.first, positionPair.second,
-        shape, red,
-        blue, green, alpha)
+        shape, color.r,
+        color.g, color.b, color.a)
 }
