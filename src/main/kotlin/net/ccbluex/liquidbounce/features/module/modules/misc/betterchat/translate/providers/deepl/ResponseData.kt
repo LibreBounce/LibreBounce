c6330393f4512c022bc4cae8e9cec077c542fa0b
DataModel <183248792+DataM0del@users.noreply.github.com>
2024-12-25 09:12:23 -0500

    refactor: use `SerializedName` and rename to `detectedSourceLanguage` to fix detekt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/betterchat/translate/providers/deepl/ResponseData.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/betterchat/translate/providers/deepl/ResponseData.kt
@@ -1,9 +1,11 @@
 package net.ccbluex.liquidbounce.features.module.modules.misc.betterchat.translate.providers.deepl
+import com.google.gson.annotations.SerializedName
 
 interface ResponseData {
     val translations: List<Translations>
 }
 interface Translations {
-    val detected_source_language: String
+    @get:SerializedName("detected_source_language")
+    val detectedSourceLanguage: String
     val text: String
 }
