d7ea9ad0f9f8389895654c433417ac6dc4eaf977
DataM0del <183248792+DataM0del@users.noreply.github.com>
2025-01-08 10:11:15 -0600

    chore(SpeedAAC5): remove semicolons

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/speed/modes/aac/SpeedAAC5.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/speed/modes/aac/SpeedAAC5.kt
@@ -17,21 +17,21 @@ class SpeedAAC5(override val parent: ChoiceConfigurable<*>) : Choice("AAC5") {
             return@handler
 
         if (player.isOnGround) {
-            event.jump = true;
-            Timer.requestTimerSpeed(0.9385f, Priority.IMPORTANT_FOR_USAGE_1, ModuleSpeed, 1);
-            speedInAir = 0.0201f;
+            event.jump = true
+            Timer.requestTimerSpeed(0.9385f, Priority.IMPORTANT_FOR_USAGE_1, ModuleSpeed, 1)
+            speedInAir = 0.0201f
         } else {
             player.speed = speedInAir
         }
         if (player.fallDistance < 2.5) {
             if (player.fallDistance > 0.7) {
                 if (player.age % 3 == 0) {
-                    Timer.requestTimerSpeed(1.925f, Priority.IMPORTANT_FOR_USAGE_1, ModuleSpeed, 1);
+                    Timer.requestTimerSpeed(1.925f, Priority.IMPORTANT_FOR_USAGE_1, ModuleSpeed, 1)
                 } else if (player.fallDistance < 1.25) {
-                    Timer.requestTimerSpeed(1.7975f, Priority.IMPORTANT_FOR_USAGE_1, ModuleSpeed, 1);
+                    Timer.requestTimerSpeed(1.7975f, Priority.IMPORTANT_FOR_USAGE_1, ModuleSpeed, 1)
                 }
             }
-            speedInAir = 0.02f;
+            speedInAir = 0.02f
         }
     } }
 }
