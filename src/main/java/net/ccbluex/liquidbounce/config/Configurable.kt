d288cf77be6a60ed27bec0774f1eb1b01be9457f
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-01-25 12:48:04 +0800

    rename

--- src/main/java/net/ccbluex/liquidbounce/config/Configurable.kt
+++ src/main/java/net/ccbluex/liquidbounce/config/Configurable.kt
@@ -25,12 +25,12 @@ open class Configurable(
 
     fun addValue(value: Value<*>) = apply {
         get().add(value)
-        value.parent = this
+        value.owner = this
     }
 
     fun addValues(values: Collection<Value<*>>) = apply {
         get().addAll(values)
-        values.forEach { it.parent = this }
+        values.forEach { it.owner = this }
     }
 
     operator fun <T, V : Value<T>> V.unaryPlus() = apply(::addValue)
--- src/main/java/net/ccbluex/liquidbounce/config/Value.kt
+++ src/main/java/net/ccbluex/liquidbounce/config/Value.kt
@@ -23,9 +23,9 @@ sealed class Value<T>(
 ) : ReadWriteProperty<Any?, T> {
 
     /**
-     * The parent of this value.
+     * The owner of this value.
      */
-    var parent: Value<*>? = null
+    var owner: Value<*>? = null
 
     /**
      * Whether this value should be excluded from public configuration (text config)
@@ -134,7 +134,7 @@ sealed class Value<T>(
 
     private var supportCondition = { true }
 
-    fun isSupported(): Boolean = (parent == null || parent!!.isSupported()) && supportCondition.invoke()
+    fun isSupported(): Boolean = (owner == null || owner!!.isSupported()) && supportCondition.invoke()
 
     fun setSupport(condition: (Boolean) -> Boolean) = apply {
         val oldCondition = supportCondition
