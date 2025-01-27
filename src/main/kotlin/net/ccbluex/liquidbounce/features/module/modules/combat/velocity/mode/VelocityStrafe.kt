e9a92a41f8bef3655a36d61056e182faa491277e
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2025-01-27 11:48:21 +0700

    feat(Velocity): Strafe OnlyFacing option

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/velocity/mode/VelocityStrafe.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/velocity/mode/VelocityStrafe.kt
@@ -18,10 +18,15 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.combat.velocity.mode
 
+import net.ccbluex.liquidbounce.config.types.ToggleableConfigurable
 import net.ccbluex.liquidbounce.event.events.PacketEvent
 import net.ccbluex.liquidbounce.event.events.PlayerMoveEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.event.sequenceHandler
+import net.ccbluex.liquidbounce.utils.aiming.RotationManager
+import net.ccbluex.liquidbounce.utils.aiming.facingEnemy
+import net.ccbluex.liquidbounce.utils.combat.findEnemy
+import net.ccbluex.liquidbounce.utils.entity.rotation
 import net.ccbluex.liquidbounce.utils.entity.sqrtSpeed
 import net.ccbluex.liquidbounce.utils.entity.withStrafe
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket
@@ -34,6 +39,15 @@ internal object VelocityStrafe : VelocityMode("Strafe") {
 
     private val delay by int("Delay", 2, 0..10, "ticks")
     private val strength by float("Strength", 1f, 0.1f..2f)
+
+    object OnlyFacing: ToggleableConfigurable(this, "OnlyFacing", false) {
+        val range by float("Range", 3.5f, 0.1f..6f)
+    }
+
+    init {
+        tree(OnlyFacing)
+    }
+
     private val untilGround by boolean("UntilGround", false)
 
     private var applyStrafe = false
@@ -44,6 +58,17 @@ internal object VelocityStrafe : VelocityMode("Strafe") {
 
         // Check if this is a regular velocity update
         if ((packet is EntityVelocityUpdateS2CPacket && packet.entityId == player.id) || packet is ExplosionS2CPacket) {
+            val target = world.findEnemy(0f..OnlyFacing.range)
+            val isFacingEnemy = facingEnemy(
+                target!!,
+                OnlyFacing.range.toDouble(),
+                RotationManager.currentRotation ?: player.rotation
+            )
+
+            if (OnlyFacing.enabled && !isFacingEnemy) {
+                return@sequenceHandler
+            }
+
             // A few anti-cheats can be easily tricked by applying the velocity a few ticks after being damaged
             waitTicks(delay)
 
