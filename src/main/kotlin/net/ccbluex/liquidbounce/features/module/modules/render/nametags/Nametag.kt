3346e3f64b13ed6c1786e8847c97382dde23ac23
DataM0del <183248792+DataM0del@users.noreply.github.com>
2025-02-07 10:54:48 -0600

    fix: import handItems extension where handItems is used

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/nametags/Nametag.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/nametags/Nametag.kt
@@ -19,6 +19,7 @@
 package net.ccbluex.liquidbounce.features.module.modules.render.nametags
 
 import net.ccbluex.liquidbounce.render.engine.Vec3
+import net.ccbluex.liquidbounce.utils.entity.handItems
 import net.ccbluex.liquidbounce.utils.entity.interpolateCurrentPosition
 import net.ccbluex.liquidbounce.utils.render.WorldToScreen
 import net.minecraft.entity.Entity
