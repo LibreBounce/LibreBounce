3d8fa3146d35d600d0a505ece7b5750703ddf953
DataM0del <183248792+DataM0del@users.noreply.github.com>
2025-02-06 22:09:24 -0600

    fix(ArmorPiece, DisablerVanillaSpeed, SpecialItemExtension, SubmoduleIdPredict): errors

--- src/main/kotlin/net/ccbluex/liquidbounce/utils/item/ArmorPiece.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/utils/item/ArmorPiece.kt
@@ -21,11 +21,10 @@ package net.ccbluex.liquidbounce.utils.item
 import net.ccbluex.liquidbounce.utils.inventory.ItemSlot
 import net.ccbluex.liquidbounce.features.module.modules.player.invcleaner.ItemSlotType
 import net.minecraft.entity.EquipmentSlot
-import net.minecraft.item.ArmorItem
 
 class ArmorPiece(val itemSlot: ItemSlot) {
     val slotType: EquipmentSlot
-        get() = (itemSlot.itemStack.item as ArmorItem).type().equipmentSlot
+        get() = itemSlot.itemStack.item.type().equipmentSlot
     val entitySlotId: Int
         get() = this.slotType.entitySlotId
     val inventorySlot: Int
@@ -36,10 +35,10 @@ class ArmorPiece(val itemSlot: ItemSlot) {
         get() = itemSlot.slotType == ItemSlotType.HOTBAR
 
     val toughness: Float
-        get() = (itemSlot.itemStack.item as ArmorItem).material().toughness
+        get() = itemSlot.itemStack.item.material().toughness
     val defensePoints: Float
         get() {
-            val item = itemSlot.itemStack.item as ArmorItem
+            val item = itemSlot.itemStack.item
 
             return item.material().defense.getOrDefault(item.type(), 0).toFloat()
         }
