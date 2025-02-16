4c5b4b2aae829e3c6cb46e8962d5b6948346ceb8
Izuna <izuna.seikatsu@ccbluex.net>
2025-02-16 13:37:16 +0100

    feat: non-combat filtering and more data

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/debugrecorder/modes/AimDebugRecorder.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/debugrecorder/modes/AimDebugRecorder.kt
@@ -25,12 +25,15 @@ import com.google.gson.JsonObject
 import net.ccbluex.liquidbounce.config.gson.util.json
 import net.ccbluex.liquidbounce.event.tickHandler
 import net.ccbluex.liquidbounce.features.module.modules.misc.debugrecorder.ModuleDebugRecorder
+import net.ccbluex.liquidbounce.utils.aiming.RotationManager
 import net.ccbluex.liquidbounce.utils.aiming.data.Rotation
 import net.ccbluex.liquidbounce.utils.client.FloatValueProvider
+import net.ccbluex.liquidbounce.utils.client.chat
 import net.ccbluex.liquidbounce.utils.combat.TargetPriority
 import net.ccbluex.liquidbounce.utils.combat.TargetTracker
 import net.ccbluex.liquidbounce.utils.entity.lastRotation
 import net.ccbluex.liquidbounce.utils.entity.rotation
+import net.ccbluex.liquidbounce.utils.entity.sqrtSpeed
 import net.ccbluex.liquidbounce.utils.entity.squaredBoxedDistanceTo
 import net.minecraft.util.hit.EntityHitResult
 import net.minecraft.util.hit.HitResult
@@ -41,30 +44,56 @@ object AimDebugRecorder : ModuleDebugRecorder.DebugRecorderMode("Aim") {
 
     @Suppress("unused")
     private val tickHandler = tickHandler {
-        val playerRotation = player.rotation
-        val playerLastRotation = player.lastRotation
+        // Record only when mouse-button is down, this will filter out bad non-combat data
+        if (!mc.options.attackKey.isPressed || player.isDead || player.health <= 0) {
+            return@tickHandler
+        }
+
+        val playerRotation = RotationManager.currentRotation ?: player.rotation
+        val playerLastRotation = RotationManager.previousRotation ?: player.lastRotation
         val target = targetTracker.selectFirst() ?: return@tickHandler
-        val turnSpeed = playerLastRotation.rotationDeltaTo(playerRotation)
+        val delta = playerLastRotation.rotationDeltaTo(playerRotation)
 
         recordPacket(JsonObject().apply {
-            val cDirVec = player.rotation.directionVector
+            // Player Data
+            val cDirVec = playerRotation.directionVector
             add("cv", json {
                 "x" to cDirVec.x
                 "y" to cDirVec.y
                 "z" to cDirVec.z
             })
+            chat("${delta.deltaYaw} ${delta.deltaPitch}")
+
+
+            val velocity = player.velocity
+            add("pv", json {
+                "x" to velocity.x
+                "y" to velocity.y
+                "z" to velocity.z
+            })
+            addProperty("s", player.sqrtSpeed)
+
+            // Target Data
+            addProperty("dist", player.squaredBoxedDistanceTo(target))
+            val offset = player.pos.subtract(target.pos)
+            add("o", json {
+                "x" to offset.x
+                "y" to offset.y
+                "z" to offset.z
+            })
+
+            // Rotation Data
             val rotation = Rotation.lookingAt(point = target.eyePos, from = player.eyePos)
-            val wDirVec = rotation.directionVector
+            val tDirVec = rotation.directionVector
             add("tv", json {
-                "x" to wDirVec.x
-                "y" to wDirVec.y
-                "z" to wDirVec.z
+                "x" to tDirVec.x
+                "y" to tDirVec.y
+                "z" to tDirVec.z
             })
             add("d", json {
-                "x" to turnSpeed.deltaYaw
-                "y" to turnSpeed.deltaPitch
+                "x" to delta.deltaYaw
+                "y" to delta.deltaPitch
             })
-            addProperty("dist", player.squaredBoxedDistanceTo(target))
 
             val crosshairTarget = mc.crosshairTarget
             addProperty("ch",
