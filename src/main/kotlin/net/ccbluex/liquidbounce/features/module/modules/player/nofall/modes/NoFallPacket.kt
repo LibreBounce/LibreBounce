0379f724162824fe740c2dd59a56d70d4cc7b1b5
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2025-02-21 23:56:34 -0700

    start at 1

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/nofall/modes/NoFallPacket.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/nofall/modes/NoFallPacket.kt
@@ -93,7 +93,7 @@ internal object NoFallPacket : Choice("Packet") {
                         )
                     )
 
-                    for (i in 0..predictionTicks) {
+                    for (i in 1..predictionTicks) {
                         simulatedPlayer.tick()
                         if (simulatedPlayer.fallDistance >= distance.activeChoice.value) {
                             event.action = Action.QUEUE
