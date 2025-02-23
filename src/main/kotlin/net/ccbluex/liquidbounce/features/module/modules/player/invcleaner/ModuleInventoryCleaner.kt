560a5d6f397af6a54bdba8926dac9e7588ae2aa8
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-02-23 10:57:42 +0800

    more aliases

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/invcleaner/ModuleInventoryCleaner.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/invcleaner/ModuleInventoryCleaner.kt
@@ -33,8 +33,9 @@ import net.minecraft.screen.slot.SlotActionType
  *
  * Automatically throws away useless items and sorts them.
  */
-object ModuleInventoryCleaner : ClientModule("InventoryCleaner", Category.PLAYER, 
-    aliases = arrayOf("Inventory Manager")) {
+object ModuleInventoryCleaner : ClientModule("InventoryCleaner", Category.PLAYER,
+    aliases = arrayOf("InventoryManager")
+) {
 
     private val inventoryConstraints = tree(PlayerInventoryConstraints())
 
