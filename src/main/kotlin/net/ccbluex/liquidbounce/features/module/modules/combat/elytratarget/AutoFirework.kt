87e38f9f65abfd3ce438af5130c000e9dfae37b8
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-03-01 13:36:38 +0800

    autofirework sync with killaura

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/elytratarget/AutoFirework.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/elytratarget/AutoFirework.kt
@@ -1,13 +1,17 @@
 package net.ccbluex.liquidbounce.features.module.modules.combat.elytratarget
 
 import net.ccbluex.liquidbounce.config.types.ToggleableConfigurable
+import net.ccbluex.liquidbounce.event.Sequence
 import net.ccbluex.liquidbounce.event.tickHandler
 import net.ccbluex.liquidbounce.features.module.modules.combat.elytratarget.ModuleElytraTarget.targetTracker
+import net.ccbluex.liquidbounce.features.module.modules.combat.killaura.ModuleKillAura
 import net.ccbluex.liquidbounce.utils.client.Chronometer
 import net.ccbluex.liquidbounce.utils.entity.squaredBoxedDistanceTo
 import net.ccbluex.liquidbounce.utils.inventory.OffHandSlot
 import net.ccbluex.liquidbounce.utils.inventory.Slots
 import net.minecraft.item.Items
+
+private const val MILLISECONDS_PER_TICK = 50
 
 /**
  * Initial firework cooldown
@@ -24,17 +28,37 @@ private inline val fireworkSlot
 
 private val fireworkChronometer = Chronometer()
 
+@Suppress("MagicNumber")
 internal object AutoFirework : ToggleableConfigurable(ModuleElytraTarget, "AutoFirework", true) {
     private val useMode by enumChoice("UseMode", FireworkUseMode.PACKET)
     private val extraDistance by float("ExtraDistance", 50f, 5f..100f, suffix = "m")
     private val slotResetDelay by intRange("SlotResetDelay", 0..0, 0..20, "ticks")
+    private val syncCooldownWithKillAura by boolean("SyncCooldownWithKillAura", false)
     private val cooldown by intRange("Cooldown", 8..10, 1..50, "ticks")
+
+    private suspend inline fun Sequence.canUseFirework(): Boolean {
+        if (!ModuleKillAura.running || !syncCooldownWithKillAura) {
+            return true
+        }
+
+        /*
+         * The Killaura is ready to perform the click.
+         * We can use the firework on the next tick.
+         * After killaura performed the click
+         */
+        return if (ModuleKillAura.clickScheduler.isGoingToClick) {
+            waitTicks(1)
+            true
+        } else {
+            false
+        }
+    }
 
     @Suppress("unused")
     private val autoFireworkHandler = tickHandler {
         val target = targetTracker.target ?: return@tickHandler
 
-        if (fireworkChronometer.hasElapsed((fireworkCooldown * 50).toLong())) {
+        if (fireworkChronometer.hasElapsed((fireworkCooldown * MILLISECONDS_PER_TICK).toLong()) && canUseFirework()) {
             val slot = fireworkSlot ?: return@tickHandler
             useMode.useFireworkSlot(slot, slotResetDelay.random())
             fireworkChronometer.reset()
