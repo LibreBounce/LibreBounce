9eeed79b6ad7aaa814eb8e601a31a508a9d505a3
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-03-04 21:23:13 +0800

    feat/auto-rod

---
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/autorod/ModuleAutoRod.kt
@@ -0,0 +1,14 @@
+@file:Suppress("MaxLineLength")
+package net.ccbluex.liquidbounce.features.module.modules.combat.autorod
+
+import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.ClientModule
+
+/**
+ * Auto use fishing rod to PVP.
+ *
+ * The original code was taken from [LiquidBounce Legacy](https://github.com/CCBlueX/LiquidBounce/blob/legacy/src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/AutoRod.kt).
+ *
+ * @author zyklone4096, sqlerrorthing
+ */
+object ModuleAutoRod : ClientModule("AutoRod", Category.COMBAT)
