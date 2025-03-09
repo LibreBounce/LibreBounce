b29db647991a77f7fcb797f7d2678d264a4be2d3
BlueLatios <65719723+LTBlueLatios@users.noreply.github.com>
2025-03-09 16:11:49 -0400

    Cleanup

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/velocity/mode/VelocityReversal.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/velocity/mode/VelocityReversal.kt
@@ -1,17 +1,22 @@
 package net.ccbluex.liquidbounce.features.module.modules.combat.velocity.mode
-import net.ccbluex.liquidbounce.features.module.modules.combat.killaura.ModuleKillAura
+
+import kotlin.math.abs
 import net.ccbluex.liquidbounce.event.events.PacketEvent
 import net.ccbluex.liquidbounce.event.events.PlayerTickEvent
 import net.ccbluex.liquidbounce.event.sequenceHandler
+import net.ccbluex.liquidbounce.features.module.modules.combat.killaura.ModuleKillAura
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket
 import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket
-import kotlin.math.abs
 
+/** 
+ * A velocity mode that reverses your velocity after a set amount of ticks. 
+ * Default value bypass Vulcan (3/9/25) ~ anticheat-test.com
+ */
 internal object VelocityReversal : VelocityMode("Reversal") {
-    private val delay by int("Delay", 2, 1..5, "ticks")
+    private val delay by int("Reversal Delay", 2, 1..5, "ticks")
+    private val xModifier by float("X Modifier", 0.5f, 0.1f..1.0f)
+    private val zModifier by float("Z Modifier", 0.5f, 0.1f..1.0f)
     private val requiresKillaura by boolean("RequiresKillAura", true)
-    private val xModifier by float("XModifier", 0.5f, 0.1f..1.0f)
-    private val zModifier by float("ZModifier", 0.5f, 0.1f..1.0f)
 
     private var handlingVelocity = false
     private var velocityTicks = 0
@@ -20,7 +25,10 @@ internal object VelocityReversal : VelocityMode("Reversal") {
         sequenceHandler<PacketEvent> { event ->
             val packet = event.packet
 
-            if ((packet is EntityVelocityUpdateS2CPacket && packet.entityId == player.id) || packet is ExplosionS2CPacket) {
+            if (
+                (packet is EntityVelocityUpdateS2CPacket && packet.entityId == player.id) ||
+                    packet is ExplosionS2CPacket
+            ) {
                 if (requiresKillaura && !ModuleKillAura.running) return@sequenceHandler
                 reset()
                 handlingVelocity = true
@@ -29,17 +37,19 @@ internal object VelocityReversal : VelocityMode("Reversal") {
 
         sequenceHandler<PlayerTickEvent> {
             if (handlingVelocity) {
-                if (velocityTicks++ >= delay) {
-                    player.velocity.x *= -xModifier
-                    player.velocity.z *= -zModifier
+                // We assume the velocity has reset.
+                // Idk of any edge cases where this logic would fail.
+                if (
+                    abs(player.velocity.x) == 0.0 &&
+                        abs(player.velocity.y) == 0.0 &&
+                        abs(player.velocity.z) == 0.0
+                ) {
                     reset()
                 }
 
-                // We assume the velocity has reset
-                // Idk of any edge cases where this logic would fail.
-                if (abs(player.velocity.x) == 0.0 &&
-                    abs(player.velocity.y) == 0.0 &&
-                    abs(player.velocity.z) == 0.0) {
+                if (velocityTicks++ >= delay) {
+                    player.velocity.x *= -xModifier
+                    player.velocity.z *= -zModifier
                     reset()
                 }
             }
