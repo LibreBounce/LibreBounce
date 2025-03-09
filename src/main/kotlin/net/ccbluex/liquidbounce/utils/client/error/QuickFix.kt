4a6b2cd4efcb84eb84603af83df15151e866a453
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-03-09 14:30:04 +0800

    build fix

--- src/main/kotlin/net/ccbluex/liquidbounce/utils/client/error/QuickFix.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/utils/client/error/QuickFix.kt
@@ -3,9 +3,7 @@ package net.ccbluex.liquidbounce.utils.client.error
 class Steps(
     val showStep: Boolean,
     val steps: Array<String>
-) {
-    constructor(showStep: Boolean = false, vararg steps: String) : this(showStep, steps.toList().toTypedArray())
-}
+)
 
 enum class QuickFix (
     val description: String,
@@ -14,20 +12,20 @@ enum class QuickFix (
 ) {
     JCEF_ISNT_COMPATIBLE_WITH_THAT_SYSTEM(
         "Your system isn't compatible with JCEF",
-        whatYouNeed = Steps(false,
+        whatYouNeed = Steps(false, arrayOf(
             "A 64-bit computer",
             "Windows 10 or newer, macOS 10.15 or newer, or a Linux system"
-        ),
-        whatToDo = Steps(false,
+        )),
+        whatToDo = Steps(false, arrayOf(
             "Please update your operating system to a never version"
-        )
+        ))
     ),
     DOWNLOAD_JCEF_FAILED(
         "A fatal error occurred while loading libraries required for JCEF to work",
-        whatToDo = Steps(false,
+        whatToDo = Steps(false, arrayOf(
             "Check your internet connection",
             "Use a VPN such as Cloudflare Warp or another one",
             "Check if there is free space on the disk."
-        )
+        ))
     )
 }
