a1577633e314bbf5977f15bfa41c905812312f55
localp <sliceclient123+localp@gmail.com>
2025-03-30 15:49:51 +0200

    moment

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/step/ModuleStep.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/step/ModuleStep.kt
@@ -279,8 +279,8 @@ object ModuleStep : ClientModule("Step", Category.MOVEMENT) {
         private var stepping = false
 
         private val stepHeight get() = when {
-            player.canStep(1.25) -> 1.25
             player.canStep(1.0) -> 1.0
+            player.canStep(1.25) -> 1.25
             else -> 1.5
         }
 
@@ -289,6 +289,8 @@ object ModuleStep : ClientModule("Step", Category.MOVEMENT) {
             if (player.canStep(1.5) && !stepping) {
                 val currentStepHeight = stepHeight
                 event.jump = true
+
+                chat(currentStepHeight.toString())
 
                 stepping = true
                 player.velocity.y = 0.42
