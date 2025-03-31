963937788bae2ff87b1067787570d7b63a5bba1d
sqlerrorthing <sqlerrorthing@gmail.com>
2025-03-31 17:01:28 +0800

    remove test due to mc errors

--- src/test/kotlin/net/ccbluex/liquidbounce/presetItems/InventoryPresetsMergingTests.kt
+++ src/test/kotlin/net/ccbluex/liquidbounce/presetItems/InventoryPresetsMergingTests.kt
@@ -2,7 +2,8 @@ package net.ccbluex.liquidbounce.presetItems
 
 import net.ccbluex.liquidbounce.config.types.InventoryPresetsValue
 import net.ccbluex.liquidbounce.features.inventoryPresets.InventoryPreset
-import net.ccbluex.liquidbounce.features.inventoryPresets.items.*
+import net.ccbluex.liquidbounce.features.inventoryPresets.items.NonePresetItem
+import net.ccbluex.liquidbounce.features.inventoryPresets.items.WeaponsPresetItem
 import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertNotNull
@@ -22,50 +23,5 @@ class InventoryPresetsMergingTests {
         assertNotNull(result)
 
         assertArrayEquals(result.items, second.items)
-    }
-
-    @Test
-    fun `merge when expected item is not in inventory`() {
-        val first = InventoryPreset(arrayOf(
-            NonePresetItem,
-            WeaponsPresetItem,
-            WeaponsPresetItem,
-            BlocksPresetItem,
-            BlocksPresetItem,
-            AnyPresetItem,
-            AnyPresetItem,
-            FoodPresetItem,
-            FoodPresetItem,
-            FoodPresetItem,
-        ))
-
-        val second = InventoryPreset(Array<PresetItem>(10) { NonePresetItem }.apply {
-            this[9] = AnyPresetItem
-        })
-
-        val third = InventoryPreset(Array(10) { WeaponsPresetItem })
-
-        val value = InventoryPresetsValue().apply {
-            set(listOf(first, second, third))
-        }
-
-        val result = value.merged() {
-            it != FoodPresetItem
-        }
-
-        assertNotNull(result)
-
-        assertArrayEquals(arrayOf(
-            WeaponsPresetItem,
-            WeaponsPresetItem,
-            WeaponsPresetItem,
-            BlocksPresetItem,
-            BlocksPresetItem,
-            AnyPresetItem,
-            AnyPresetItem,
-            WeaponsPresetItem,
-            WeaponsPresetItem,
-            AnyPresetItem
-        ), result.items)
     }
 }
