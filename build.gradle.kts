407c3e53cc740e74c7831ec56c5a84d8f39eea9d
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2025-08-03 16:44:49 +0100

    aa

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     idea
-    id("org.jetbrains.kotlin.jvm") version "kotlin_version"
+    kotlin("jvm")
     id("com.github.johnrengelman.shadow") version "6.1.0"
     id("net.minecraftforge.gradle.forge") version "forge_gradle_version"
     id("org.spongepowered.mixin") version "mixin_gradle_version"
