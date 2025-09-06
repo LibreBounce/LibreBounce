5ca406d5e132107d668b7ec92a2b81cc2f9ef080
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2025-09-06 12:06:36 +0100

    Hopefully fix compilation

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/CombatJump.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/CombatJump.kt
@@ -27,6 +27,9 @@ object CombatJump : Module("CombatJump", Category.COMBAT) {
 
         if (onlyMove && !player.isMoving) return@handler
 
+        val target = target
+        val targetMixin = target as? IMixinEntity
+
         val distance = player.getDistanceToEntityBox(target) ?: return@handler
 
         if (distance in targetDistance) {
