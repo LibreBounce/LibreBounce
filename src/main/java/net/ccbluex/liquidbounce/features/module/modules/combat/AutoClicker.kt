ea063e882ffdaf0961ac0e8d6d1edca4419a73d3
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2025-09-11 07:04:14 +0100

    Maybe?

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/AutoClicker.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/AutoClicker.kt
@@ -33,7 +33,7 @@ object AutoClicker : Module("AutoClicker", Category.COMBAT) {
     private val simulateDoubleClicking by boolean("SimulateDoubleClicking", false)
     private val cps by intRange("CPS", 5..8, 1..50)
 
-    private val smart by bool("Smart", false) { left }
+    private val smart by boolean("Smart", false) { left }
     private val hurtTime by int("HurtTime", 10, 0..10) { left && !smart }
 
     private val right by boolean("Right", true)
@@ -167,15 +167,16 @@ object AutoClicker : Module("AutoClicker", Category.COMBAT) {
         }
     }
 
-    private fun shouldLeftClick {
+    private val shouldLeftClick: Boolean {
         val player = mc.thePlayer ?: return false
 
         if (target != null) return true
 
+
         if (!smart && target!!.hurtTime > hurtTime) return true
 
         // Credits to Gugustus
-        if (target.hurtTime >= 2) {
+        if (target!!.hurtTime >= 2) {
         	return false
         }
 
@@ -183,19 +184,19 @@ object AutoClicker : Module("AutoClicker", Category.COMBAT) {
             return true
         }
 
-        if (target.hurtTime >= 3 && !target.onGround && player.getDistanceToEntityBox(target) >= 2.5 && player.getDistanceToEntityBox(target) >= 3.0) {
+        if (target!!.hurtTime >= 3 && !target.onGround && player.getDistanceToEntityBox(target) >= 2.5 && player.getDistanceToEntityBox(target) >= 3.0) {
             return false
         }
 
-        if (player.hurtTime == 0 && target.hurtTime >= 3) {
+        if (player.hurtTime == 0 && target!!.hurtTime >= 3) {
             return false
         }
 
-        if (player.hurtTime == 2 && target.hurtTime == 4) {
+        if (player.hurtTime == 2 && target!!.hurtTime == 4) {
             return false
-        } else if (player.hurtTime == 3 && target.hurtTime == 5) {
+        } else if (player.hurtTime == 3 && target!!.hurtTime == 5) {
             return false
-        } else if (player.hurtTime >= 5 && target.hurtTime >= 7) {
+        } else if (player.hurtTime >= 5 && target!!.hurtTime >= 7) {
             return false
         } else {
             return true
