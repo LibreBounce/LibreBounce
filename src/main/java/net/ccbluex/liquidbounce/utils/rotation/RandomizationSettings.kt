112e668eb39c0ffac69fe4b6bc578794c35ec3c3
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2025-09-12 14:36:13 +0100

    Another test

--- src/main/java/net/ccbluex/liquidbounce/utils/rotation/RandomizationSettings.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/rotation/RandomizationSettings.kt
@@ -29,10 +29,15 @@ class RandomizationSettings(owner: Module, val generalApply: () -> Boolean = { t
         "YawRandomizationRange",
         5f..10f,
         0f..30f
-    ) { isZizZagActive && randomizationChosen && yawRandomizationChance.start != 1F }
+    ) { isZigZagActive && randomizationChosen && yawRandomizationChance.start != 1F }
     private val yawSpeedIncreaseMultiplier by intRange(
         "YawSpeedIncreaseMultiplier", 50..120, 0..500, suffix = "%"
-    ) { !isZizZagActive && randomizationChosen && yawRandomizationChance.start != 1F }
+    ) { !isZigZagActive && randomizationChosen && yawRandomizationChance.start != 1F }
+    private val yawPrevSmoothingTicks by intRange(
+        "yawPrevSmoothingTicks",
+        2..2,
+        0..5
+    ) { randomizationChosen && pitchRandomizationChance.start != 1F }
     private val pitchRandomizationChance by floatRange(
         "PitchRandomizationChance", 0.8f..1.0f, 0f..1f
     ) { randomizationChosen }
@@ -41,8 +46,18 @@ class RandomizationSettings(owner: Module, val generalApply: () -> Boolean = { t
         5f..10f,
         0f..30f
     ) { randomizationChosen && pitchRandomizationChance.start != 1F }
+    private val pitchPrevSmoothingTicks by intRange(
+        "PitchPrevSmoothingTicks",
+        2..2,
+        0..5
+    ) { randomizationChosen && pitchRandomizationChance.start != 1F }
+    private val pitchPrevSmoothingTicksRandom by intRange(
+        "PitchPrevSmoothingTicksRandom",
+        -1..1,
+        -5..5
+    ) { randomizationChosen && pitchRandomizationChance.start != 1F }
 
-    private val isZizZagActive
+    private val isZigZagActive
         get() = randomizationPattern == "Zig-Zag"
 
     val randomizationChosen
@@ -53,14 +68,14 @@ class RandomizationSettings(owner: Module, val generalApply: () -> Boolean = { t
 
         // Smooth out randomized rotation pattern using previous rotation to simulate natural movement
         val pitchMovement =
-            angleDifference(rotation.pitch, lastRotations[2].pitch).sign.takeIf { it != 0f } ?: (-1..1).random()
+            angleDifference(rotation.pitch, lastRotations[pitchPrevSmoothingTicks.random()].pitch).sign.takeIf { it != 0f } ?: pitchPrevSmoothingTicksRandom.random()
                 .toFloat()
-        val yawMovement = angleDifference(rotation.yaw, lastRotations[2].yaw)
+        val yawMovement = angleDifference(rotation.yaw, lastRotations[yawPrevSmoothingTicks.random()].yaw)
 
         val yawSign = yawMovement.sign.takeIf { it != 0f } ?: arrayOf(-1f, 1f).random()
 
         val yawIncrease = if (Math.random() > yawRandomizationChance.random()) {
-            if (!isZizZagActive) {
+            if (!isZigZagActive) {
                 yawSpeedIncreaseMultiplier.random() / 100f * yawMovement
             } else {
                 yawRandomizationRange.random() * yawSign
@@ -68,14 +83,14 @@ class RandomizationSettings(owner: Module, val generalApply: () -> Boolean = { t
         } else 0f
 
         val pitchIncrease = if (Math.random() > pitchRandomizationChance.random()) {
-            if (!isZizZagActive) {
+            if (!isZigZagActive) {
                 pitchRandomizationRange.random() + pitchMovement
             } else {
                 pitchRandomizationRange.random() * pitchMovement
             }
         } else 0f
 
-        if (isZizZagActive || intercept?.hitVec == null) {
+        if (isZigZagActive || intercept?.hitVec == null) {
             rotation.yaw += yawIncrease
             rotation.pitch += pitchIncrease
 
