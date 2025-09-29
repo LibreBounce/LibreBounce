4878412b9581958e5b7c2c795630aa7d10d21eae
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2025-09-29 05:04:58 +0100

    OK

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/stepmodes/StepMode.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/stepmodes/StepMode.kt
@@ -8,6 +8,7 @@ package net.ccbluex.liquidbounce.features.module.modules.movement.stepmodes
 import net.ccbluex.liquidbounce.event.JumpEvent
 import net.ccbluex.liquidbounce.event.MoveEvent
 import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.event.StepEvent
 import net.ccbluex.liquidbounce.event.StepConfirmEvent
 import net.ccbluex.liquidbounce.utils.client.MinecraftInstance
 
@@ -15,6 +16,7 @@ open class StepMode(val modeName: String) : MinecraftInstance {
     open fun onMotion() {}
     open fun onUpdate() {}
     open fun onMove(event: MoveEvent) {}
+    open fun onStep(event: StepEvent) {}
     open fun onStepConfirm(event: StepConfirmEvent) {}
     open fun onStrafe() {}
     open fun onJump(event: JumpEvent) {}
