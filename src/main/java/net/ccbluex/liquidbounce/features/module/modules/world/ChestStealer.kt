ca3d209fd0d62d750ea2418f8800b4f1e2789b2f
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2025-10-01 07:05:08 +0100

    yay

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/ChestStealer.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/ChestStealer.kt
@@ -67,7 +67,7 @@ object ChestStealer : Module("ChestStealer", Category.WORLD) {
     // TODO: Add an option to not miss-click consecutively
     private val missClick by boolean("MissClick", false)
     private val missClickChance by int("MissClickChance", 15, 0..100, suffix = "%") { missClick }
-    private val missClickDistMult by boolean("MissClickChanceDistanceMultiply", true) { missClick }
+    private val missClickChanceDistMult by boolean("MissClickChanceDistanceMultiply", true) { missClick }
     private val pauseAfterMissClick by intRange("PauseAfterMissClick", 350..650, 0..1000, suffix = "ms") { missClick }
 
     private val delay by intRange("Delay", 50..50, 0..500, suffix = "ms")
