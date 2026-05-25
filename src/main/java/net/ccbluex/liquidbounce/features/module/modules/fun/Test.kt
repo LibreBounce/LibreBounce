b292e7b253943d72d80b2f221a8d7f20fba06aff
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2026-05-25 11:12:53 +0100

    ttt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/fun/Test.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/fun/Test.kt
@@ -43,7 +43,7 @@ object Test : Module("Test", Category.FUN, subjective = true) {
     val onUpdate = handler<UpdateEvent> { event ->
         val player = mc.thePlayer ?: return@handler
 
-        val fixedTarget = (KillAura.target as Entity) ?: target ?: return@handler
+        val fixedTarget = (KillAura.target as Entity) ?: (target as Entity) ?: return@handler
 
         if (checkFakeLagging) checkFakeLagging(fixedTarget)
     }
