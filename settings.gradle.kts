42f292cc4865089b8fdf8453a18bd2f40ab1cbe5
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2026-08-02 08:22:10 +0100

    ok

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,11 @@
 pluginManagement {
     repositories {
-        maven {
-            name = "Fabric"
-            url = uri("https://maven.fabricmc.net/")
-        }
-        maven {
-            name = "Ornithe Releases"
-            url = uri("https://maven.ornithemc.net/releases")
-        }
-        maven {
-            name = "Ornithe Snapshots"
-            url = uri("https://maven.ornithemc.net/snapshots")
-        }
-        maven {
-            name = "Jitpack"
-            url = uri("https://jitpack.io/")
-        }
-        mavenCentral()
         gradlePluginPortal()
-    }
-
-    plugins {
-        id("fabric-loom") version providers.gradleProperty("loom_version")
-        id("ploceus") version providers.gradleProperty("ploceus_version")
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.ornithemc.net/releases")
+        maven("https://maven.ornithemc.net/snapshots")
+        maven("https://jitpack.io/")
+        mavenCentral()
     }
 }
 
