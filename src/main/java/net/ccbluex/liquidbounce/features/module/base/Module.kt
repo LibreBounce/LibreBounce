6e6f5626c38a62218eb006f6ceb9754811b600d5
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2026-09-08 12:35:55 +0100

    .

--- src/main/java/net/ccbluex/liquidbounce/features/module/base/Module.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/base/Module.kt
@@ -3,7 +3,7 @@
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
  * https://github.com/CCBlueX/LiquidBounce/
  */
-package net.ccbluex.liquidbounce.features.module
+package net.ccbluex.liquidbounce.features.module.base
 
 import net.ccbluex.liquidbounce.LiquidBounce.isStarting
 import net.ccbluex.liquidbounce.config.Configurable
